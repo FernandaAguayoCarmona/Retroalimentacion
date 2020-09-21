afef4d83e0734e110902e60171748c58e9675360
Fernanda Aguayo <fernanda.aguayo1323@alumnos.udg.mx>
2020-09-20 22:31:15 -0500

    Declaracion de la estructura del personaje

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+
+typedef struct personaje {char nombre [30];
+char tipo[30]; int fuerza; int salud;}personaje;
+
+int main ()
+{
+    return 0;
+}
