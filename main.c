64827f7a54db471e4157e1241533231dacdcd5ca
Fernanda Aguayo <fernanda.aguayo1323@alumnos.udg.mx>
2020-09-20 22:42:47 -0500

    Captura de los datos del personaje

--- main.c
+++ main.c
@@ -8,5 +8,32 @@ char tipo[30]; int fuerza; int salud;}personaje;
 
 int main ()
 {
+    caracter_personaje ();
+    getch ();
     return 0;
+}
+
+// Captura de datos del personaje
+void caracter_personaje()
+{
+    int i=0;
+    personaje perso[4];
+
+    for (i=0;i<5;i++)
+    {
+        printf ("\nPersonajes");
+        printf ("\nNombre del personaje:");
+        scanf ("%s",&perso[i].nombre);
+
+        printf ("\nTipo del personaje:");
+        scanf ("%s",&perso[i].tipo);
+
+        printf ("\nFuerza del personaje:");
+        scanf ("%d",&perso[i].fuerza);
+
+        printf ("\nSalud del personaje:");
+        scanf ("%d",&perso[i].salud);
+
+        system ("cls");
+    }
 }
